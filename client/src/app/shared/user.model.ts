c267880d37f2c23beb92ae39420e99ac446cb100
kundan6 <55451805+kundan6@users.noreply.github.com>
2020-04-25 18:19:09 +0530

    dddd
    sssssss

---
+++ client/src/app/shared/user.model.ts
@@ -0,0 +1,12 @@
+export class User {
+    fullName: string;
+    Name: string;
+    DisplayName:string;
+    Website:string;
+    bio:string;
+      email: string; 
+    password: string;
+    jabber:string;
+    aol:string;
+    yahoo:string;
+}
